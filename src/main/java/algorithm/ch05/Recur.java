59b855cfcb329833d5e6f9029af3314b687816b0
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-03-24 11:18:42 +0900

    재귀함수 알고리즘 분석법

---
+++ src/main/java/algorithm/ch05/Recur.java
@@ -0,0 +1,24 @@
+package algorithm.ch05;
+
+
+import java.util.Scanner;
+
+public class Recur {
+    //재귀 함수
+    static void recur(int n) {
+        if (n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            recur(n - 2);
+        }
+    }//end recur
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요 : ");
+        int x = sc.nextInt();
+
+        recur(x);
+    }//end main
+}//end class

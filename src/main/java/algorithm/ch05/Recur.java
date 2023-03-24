1e107af2e2ec06c631cac16de1fc732e8c842653
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-03-24 11:31:37 +0900

    재귀의 제거

--- src/main/java/algorithm/ch05/Recur.java
+++ src/main/java/algorithm/ch05/Recur.java
@@ -1,6 +1,8 @@
 package algorithm.ch05;
 
 
+import algorithm.ch04.IntStack;
+
 import java.util.Scanner;
 
 public class Recur {
@@ -12,11 +14,30 @@ public class Recur {
             System.out.println(n);
             recur(n - 2);
          */
-            //고리 재귀의 제거
+        //고리 재귀의 제거
+        /*
         while (n > 0) {
             recur(n - 1);
             System.out.println(n);
             n = n - 2;
+        }
+        */
+        // 재귀의 제거
+        IntStack s = new IntStack(n);
+
+        while (true) {
+            if (n > 0) {
+                s.push(n);          // n 값을 푸뷔
+                n = n - 1;
+                continue;
+            }
+            if (s.isEmpty() != true) {      //스택이 비어 있지 않다면
+                n = s.pop();                // 저장하고 있던 스택의 값을 팝
+                System.out.println(n);
+                n = n - 2;
+                continue;
+            }
+            break;
         }
     }//end recur
 

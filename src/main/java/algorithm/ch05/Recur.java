a32109fa81c2355678fcd73e4e7a217355414230
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-03-24 11:24:30 +0900

    꼬리 재귀의 제거

--- src/main/java/algorithm/ch05/Recur.java
+++ src/main/java/algorithm/ch05/Recur.java
@@ -6,10 +6,17 @@ import java.util.Scanner;
 public class Recur {
     //재귀 함수
     static void recur(int n) {
+        /*
         if (n > 0) {
             recur(n - 1);
             System.out.println(n);
             recur(n - 2);
+         */
+            //고리 재귀의 제거
+        while (n > 0) {
+            recur(n - 1);
+            System.out.println(n);
+            n = n - 2;
         }
     }//end recur
 

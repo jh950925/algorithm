2af271887c4449b5832b6cd2cf122eba43049695
jh950925 <49939169+jh950925@users.noreply.github.com>
2023-03-28 11:35:34 +0900

    단순 삽입 정렬

---
+++ src/main/java/algorithm/ch06/InsertionSort.java
@@ -0,0 +1,39 @@
+package algorithm.ch06;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+    //단순 삽입 정렬
+    static void insertionSort(int[] a, int n) {
+        for (int i = 1; i < n; i++) {
+            int j;
+            int temp = a[i];
+            for (j = i; j > 0 && a[j - 1] > temp; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = temp;
+        }
+    }//end insertionSort
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("단순 삽입 정렬");
+        System.out.print("요솟수 : ");
+        int nx = sc.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = sc.nextInt();
+        }
+
+        insertionSort(x, nx);
+
+        System.out.println("오름차순으로 정렬");
+        for (int i = 0; i < nx; i++) {
+            System.out.println("x[" + i + "] : " + x[i]);
+        }
+        sc.close();
+    }//end main
+}//end class
